3b0ddad61bcd7a4d6c8be83a9ed8f1118d3586b9
Fahd <fahd.addi@gmail.com>
2020-05-20 17:29:36 +0000

    static data

---
+++ Models/Deal.js
@@ -0,0 +1,12 @@
+class Deal {
+    constructor(id, title, price, oldPrice, description, image) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.oldPrice = oldPrice;
+        this.description = description;
+        this.image = image;
+    }
+}
+
+export default Deal;
