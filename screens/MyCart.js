de93b4210f2ae979517af042deffd7ca1425a924
Fahd <fahd.addi@gmail.com>
2020-05-27 13:33:11 +0000

    adding Toast android + order button design

--- screens/MyCart.js
+++ screens/MyCart.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { View, ScrollView, Text, StyleSheet } from "react-native";
+import {
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+  Platform,
+  ToastAndroid,
+} from "react-native";
+import { Root, Toast } from "native-base";
 import { useSelector } from "react-redux";
 
 // importing constants
@@ -8,21 +16,55 @@ import Colors from "../constants/Colors";
 
 // importing components
 import CartItem from "../components/cart/Item";
+import FaButton from "../components/Button";
 
 const MyCart = (props) => {
   const CartItems = useSelector((state) => state.myCart.cart);
-  const [loading, setLoading] = useState(true);
 
   const renderItem = (item) => {
     return <CartItem product={item} key={`cart_item_${item.id}`} />;
   };
-
+  if (CartItems.length == 0) {
+    return (
+      <View style={{ ...styles.screen, ...styles.emptyScreen }}>
+        <Text style={styles.emptyText}>Your cart is empty</Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.screen}>
-      <ScrollView>
-        <Text>Home screen</Text>
-        {CartItems.map((item) => renderItem(item))}
-      </ScrollView>
+      <Root>
+        <ScrollView>{CartItems.map((item) => renderItem(item))}</ScrollView>
+        <View style={styles.footer}>
+          <View style={styles.footerButtonContainer}>
+            <FaButton
+              onPress={() => {
+                if (Platform.OS === "android") {
+                  ToastAndroid.show(
+                    "this App is only for demo purpose!",
+                    ToastAndroid.SHORT
+                  );
+                } else {
+                  Toast.show({
+                    text: "this App is only for demo purpose!",
+                    buttonText: "Okay",
+                    textStyle: {
+                      color: Colors.white,
+                      fontSize: CustomStyle.fontSize.xsmall,
+                      fontFamily: "default",
+                      textAlign: "center",
+                    },
+                    duration: 3000,
+                  });
+                }
+              }}
+              style={styles.footerButton}
+            >
+              Order
+            </FaButton>
+          </View>
+        </View>
+      </Root>
     </View>
   );
 };
@@ -33,6 +75,29 @@ const styles = StyleSheet.create({
     paddingHorizontal: CustomStyle.screen.paddingHorizontal,
     paddingVertical: CustomStyle.screen.paddingVertical,
     backgroundColor: Colors.background,
+  },
+  emptyScreen: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  emptyText: {
+    color: Colors.primary,
+    fontFamily: "default-black",
+  },
+  footer: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderTopWidth: 1,
+    borderTopColor: Colors.grey_light,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  footerButtonContainer: {
+    width: "100%",
+  },
+  footerButton: {
+    backgroundColor: Colors.black,
   },
 });
 
